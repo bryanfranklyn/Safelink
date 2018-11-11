c58e90dba744b7ec0f110faaac31a7c46eddb45b
bryanfranklyn <bryan.kapahang@gmail.com>
2018-11-12 01:01:57 +0800

    Update SLrandom.js

--- SLrandom.js
+++ SLrandom.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2018/09/apa-itu-fiber-optik-penjelasan-fiber.html"}
-if(x=="2"){xxx="/2018/09/struktur-umum-kabel-fiber-optik.html"}
-if(x=="3"){xxx="/2018/09/4-jenis-fiber-optik.html"}
-if(x=="4"){xxx="/2018/09/komponen-komponen-fiber-optik.html"}
+if(x=="1"){xxx="/2018/11/10-cara-merawat-muka-berjerawat.html"}
+if(x=="2"){xxx="/2018/11/best-auto-insurance-companies-in.html"}
+if(x=="3"){xxx="/2018/11/great-to-learn-about-automobile.html"}
+if(x=="4"){xxx="/2018/11/cara-menghilangkan-jerawat-dan-bekasnya.html"}
 if(x=="5"){xxx="/p/generate.html"}a_to_ve[i].href="https://widget4fre3.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
